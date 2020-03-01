1cccb382d53cf8967ca4b2bac6a9ca4260f474bf
SamirIngley <samir.ingle7@gmail.com>
2020-03-01 11:30:55 -0800

    profile page and login log out work

--- users/views.py
+++ users/views.py
@@ -11,13 +11,16 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username}')
-            return redirect('post-list-page')
+            messages.success(request, f'{username} account has been created! Log in')
+            return redirect('login')
     else: 
         form = UserRegisterForm()
 
     return render(request, 'users/register.html', {'form':form})
 
+
+def profile(request):
+    return render(request, 'users/profile.html')
 
 
 # message.debug
