c2ef7999364a9881a98b3fc0e934e96d5fb356e5
SamirIngley <samir.ingle7@gmail.com>
2020-03-06 15:08:35 -0800

    CRUD Functionality all works

--- post/urls.py
+++ post/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from .views import PostListView, PostDetailView, PostCreateView
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
 
 
 urlpatterns = [
@@ -7,5 +7,7 @@ urlpatterns = [
     path('', PostListView.as_view(), name='post-list-page'),
     path('new/', PostCreateView.as_view(), name='post-create-page'),
     path('<int:pk>/', PostDetailView.as_view(), name='post-detail-page'),
+    path('<int:pk>/update', PostUpdateView.as_view(), name='post-update-page'),
+    path('<int:pk>/delete', PostDeleteView.as_view(), name='post-delete-page'),
 
 ]
--- post/views.py
+++ post/views.py
@@ -2,36 +2,65 @@ from django.shortcuts import render
 from django.views.generic import (
      ListView, 
      CreateView,
-     DetailView 
+     DetailView,
+     UpdateView,
+     DeleteView
 )
 
 from django.http import HttpResponseRedirect
 from django.urls import reverse_lazy
-
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from .models import Post
 # Create your views here.
 
 #  naming convenvtion:
-#  <app>/<model>_<viewtype>.html 
-#  
+#  <app>/<model>_<viewtype>.html  
 
-class PostListView(ListView):
+class PostListView(LoginRequiredMixin, ListView):
     model = Post
     # template_name ='post/list.html'
     context_object_name = 'posts'
     ordering = ['-date_posted']
 
-class PostCreateView(CreateView):
+class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
     fields = ['title', 'description']
+    # success_url = ('post-list-page')
 
     def form_valid(self, form):
         form.instance.author = self.request.user  # set author to current logged in user
         return super().form_valid(form)
 
-
-class PostDetailView(DetailView):
+class PostDetailView(LoginRequiredMixin, DetailView):
     model = Post
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView,):
+    model = Post
+    fields = ['title', 'description']
+    # success_url = ('post-list-page')
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user  # set author to current logged in user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+class PostDeleteView(DeleteView, LoginRequiredMixin, UserPassesTestMixin):
+    model = Post
+    success_url = ('/post')
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+    
+
 
     # def get(self, request, slug):
     #     ''' Renders specific post via slug.'''
