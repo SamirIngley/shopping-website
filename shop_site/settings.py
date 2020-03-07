6fbdb59f259abe20c15ae1ba4c67dd3b0583af39
SamirIngley <samir.ingle7@gmail.com>
2020-03-06 18:26:21 -0800

    secret key issues

--- shop_site/settings.py
+++ shop_site/settings.py
@@ -20,8 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-with open('/etc/secret_key.txt') as f:
-    SECRET_KEY = f.read().strip()
+SECRET_KEY = '0l=sj48(2m9e=+#3m8_8k1nhx#_y=bw_g4unt*m9w&uv%i&-i5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
